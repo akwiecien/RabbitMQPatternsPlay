fdcedb7c94bcb546afb0112acecb44b86568fe74
Andrzej Kwiecien <kwiecien_andrzej@interia.pl>
2022-09-24 17:07:08 +0200

    Request/Reply

---
+++ RequestReply/Publisher/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var dots = new List<string> { "Kacper", "Piotr", "Ania" };
+
+        for (int i = 0; i < 3; i++)
+        {
+            var message = $"{dots[i]}";
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //prepare for response receiving
+                //we can use anonymous queue to handle response. 
+                var replyQueueName = channel.QueueDeclare().QueueName;
+                var consumer = new EventingBasicConsumer(channel);
+                var correlationId = Guid.NewGuid().ToString();
+
+                var props = channel.CreateBasicProperties();
+                props.CorrelationId = correlationId;
+                props.ReplyTo = replyQueueName;
+
+                //create consumer for response receiving:
+                consumer.Received += (model, ea) =>
+                {
+                    if (ea.BasicProperties.CorrelationId == correlationId)
+                    {
+                        var body = ea.Body.ToArray();
+                        var response = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($"Response has returned: {response}");
+                    }
+                };
+                channel.BasicConsume(queue: replyQueueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                //and now send message
+                var queueName = "UpperCaseReplayQueue";
+                channel.QueueDeclare(queue: queueName,
+                            durable: true,
+                            exclusive: false,
+                            autoDelete: false);
+
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "",
+                                    routingKey: queueName,
+                                    basicProperties: props,
+                                    body: body);
+
+                Console.WriteLine($"Message has been send: {message}");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
---
+++ RequestReply/Reply/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            var queueName = "UpperCaseReplayQueue";
+            channel.QueueDeclare(queue: queueName,
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false);
+
+            //Get message
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var props = ea.BasicProperties;
+                var replayProps = channel.CreateBasicProperties();
+
+                string response = string.Empty;
+                try
+                {
+                    replayProps.CorrelationId = props.CorrelationId;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received: {0}", message);
+                    response = message.ToUpper();                    
+                }
+                catch (System.Exception)
+                {
+                    response = "error";
+                }
+                finally 
+                {
+                    var responseSerialized = Encoding.UTF8.GetBytes(response);
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: props.ReplyTo,
+                                         basicProperties: replayProps,
+                                         body: responseSerialized);
+                    Console.WriteLine(" [x] Responded: {0}", response);
+                    //manual ack
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag,
+                                     multiple: false);
+                }    
+            };
+
+            channel.BasicConsume(queue: queueName,
+              autoAck: false, consumer: consumer);
+
+            //exit
+            Console.ReadLine();
+        }
+    }
+}
