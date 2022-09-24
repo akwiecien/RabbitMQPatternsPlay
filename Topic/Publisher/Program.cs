84dcb647e909ccef425f174425a9656709005a8b
Andrzej Kwiecien <kwiecien_andrzej@interia.pl>
2022-09-24 11:23:53 +0200

    Topic Pattern

---
+++ Topic/Publisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // var dots = new List<string>{"",".","..","...","....","....."};
+        var dots = new List<string>{"Pl.En.De","Pl.En.*","Pl.*.*"};
+
+        for (int i = 0; i < 3; i++)
+        {
+            var message = $"Wiadomosc:  {dots[i]}";
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic",
+                                        type: ExchangeType.Topic,
+                                        durable: true,
+                                        autoDelete: false,
+                                        arguments: null);
+
+
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "topic",
+                                    routingKey: dots[i],
+                                    basicProperties: null,
+                                    body: body);
+
+                Console.WriteLine($"Message has been send: {message}");
+            }
+        }
+
+        Console.WriteLine(" Press any key to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ Topic/ReceiverPl/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "topic",
+                                    type: ExchangeType.Topic,
+                                    durable: true,
+                                    autoDelete: false,
+                                    arguments: null);
+
+            var queueName = "PlQueue";
+            channel.QueueDeclare(queue: queueName,
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false);
+
+
+            channel.QueueBind(queue: queueName,
+                              exchange: "topic",
+                              routingKey: "Pl.*.*");
+
+
+            //Get message
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received: {0}", message);
+
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
+                Console.WriteLine(" [x] Done");
+
+                //manual ack
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+            // Acknowledge - manual
+            channel.BasicConsume(queue: queueName,
+                                 autoAck: false,
+                                 consumer: consumer);
+
+            //exit
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
