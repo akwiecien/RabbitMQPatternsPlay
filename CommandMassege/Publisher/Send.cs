392b458bb53c4ad71a67b54cea9a1080e41a3aec
Andrzej Kwiecien <kwiecien_andrzej@interia.pl>
2022-09-24 20:25:32 +0200

    Command Pattern

---
+++ CommandMassege/Publisher/Send.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "command",
+                                durable: false,  // the queue and messages will survive a broker restart
+                                exclusive: false,  //used only by one connection. will be delete when connection is closed
+                                autoDelete: false,     // queue will be deleted if last consumer unsubscribe
+                                arguments: null);      //additional arguments
+
+            // passing command via message
+            string message = $"sql command";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                routingKey: "command",
+                                basicProperties: null,
+                                body: body);
+
+            System.Console.WriteLine($"Message has been send: {message}");
+        }
+
+
+        Console.WriteLine(" Press any key to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ CommandMassege/Receiver/Receive.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "command",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+    //Get message
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [x] Received: {0}", message);
+    };
+    // Acknowledge
+    channel.BasicConsume(queue: "command",
+                         autoAck: true,
+                         consumer: consumer);
+
+    //exit
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
