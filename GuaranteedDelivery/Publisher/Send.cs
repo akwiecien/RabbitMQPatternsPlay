6d0838c77276a968c45f6b4cb917abfccd8527c2
Andrzej Kwiecien <kwiecien_andrzej@interia.pl>
2022-09-25 21:23:54 +0200

    Guaranteed delivery

---
+++ GuaranteedDelivery/Publisher/Send.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using RabbitMQ.Client;
+using System.Text;
+using Newtonsoft.Json;
+
+for (int i = 0; i < 10; i++)
+{
+    Thread.Sleep(1000);
+    var factory = new ConnectionFactory() { HostName = "localhost" };
+    using (var connection = factory.CreateConnection())
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: "guaranteed-delivery-queue",
+                            durable: false,  // the queue and messages will survive a broker restart
+                            exclusive: false,  //used only by one connection. will be delete when connection is closed
+                            autoDelete: false,     // queue will be deleted if last consumer unsubscribe
+                            arguments: null);      //additional arguments
+
+        var message = new 
+        {
+            Id = Guid.NewGuid(),
+            Content = "Message"
+        };
+        
+        //before send we save message in database - if sender, broker or receiver fail our message will not be lost
+        //_repository.Save(message);
+
+        var serializedObject = JsonConvert.SerializeObject(message);
+        var body = Encoding.UTF8.GetBytes(serializedObject);
+       
+
+        channel.BasicPublish(exchange: "",
+                            routingKey: "guaranteed-delivery-queue",
+                            basicProperties: null,
+                            body: body);
+
+        System.Console.WriteLine($"Message has been send: {message}");
+    }
+}
+
+
+Console.WriteLine(" Press any key to exit.");
+Console.ReadLine();
---
+++ GuaranteedDelivery/Receiver/Receive.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using Newtonsoft.Json;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "guaranteed-delivery-queue",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+    //Get message
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var serializedMessage = Encoding.UTF8.GetString(body);
+        var message = JsonConvert.DeserializeObject<object>(serializedMessage);
+
+        //after job is finished we cen remove it or mark as finished in database
+        // _repository.Delete(message.Id)
+        
+        Console.WriteLine(" [x] Received: {0}", message);
+    };
+    // Acknowledge
+    channel.BasicConsume(queue: "guaranteed-delivery-queue",
+                         autoAck: true,
+                         consumer: consumer);
+
+    //exit
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
